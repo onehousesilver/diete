806c841bf3f084b0705c137dc0951f274f5133b9
onehousesilver <onehousesilver@naver.com>
2022-03-17 02:23:24 +0900

    Feat: 배너 추가

--- Client/src/components/Main/BannerPage.vue
+++ Client/src/components/Main/BannerPage.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>banner</h1>
+    <div class="banner">
+      <div class="banner-head">
+        <span class="di" style="color: #2bc0af">di</span>
+        <span class="e" style="color: #25ab9b">è</span>
+        <span class="te" style="color: #219285">te</span>
+      </div>
+      <div class="banner-sub-head">
+        <span>개인별 맞춤형 식단과 음식을 추천해드립니다.</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +17,25 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.banner {
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url("../../assets/bannerimg.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.banner .banner-head {
+  font-size: 60px;
+  font-weight: 700;
+}
+
+.banner .banner-sub-head {
+  font-size: 18px;
+  color: #333;
+}
+</style>
