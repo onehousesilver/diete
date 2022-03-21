4ab408e4d5298730c45f8830c53fac754a6b6545
kst2575 <kst2575@naver.com>
2022-03-21 17:45:08 +0900

    Feat: search 기능 구현 중

--- Server/server/settings.py
+++ Server/server/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'records',
 
     # Third party apps
-    
+    'rest_framework',
+
     # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -135,3 +136,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    )
+}
+
+import datetime
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=30),
+}
