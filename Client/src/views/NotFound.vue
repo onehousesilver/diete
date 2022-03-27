2853426a2eca5c954af8c9da99b9b687812134e3
asooso1 <asooso1@naver.com>
2022-03-27 22:26:30 +0900

    Feat: 라우터 메타필드(requiresAuth)추가, 비회원 접근 차단로직 추가, 404페이지 추가

---
+++ Client/src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>페이지를 찾을 수 없습니다. <br /> 잠시 후 최초 페이지로 이동합니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  methods: {
+    // 5초 후 랜딩페이지로 이동
+    goToHomeAfterFiveSec(){
+      setTimeout(() => {
+        this.$router.push({ name: 'home' })
+      }, 5000)
+    }
+  },
+  mounted() {
+    this.goToHomeAfterFiveSec()
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  top: 30vh;
+  position: relative;
+}
+
+</style>
