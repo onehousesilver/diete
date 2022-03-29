26045c9601148abb984f419de8530372b8041811
kst2575 <kst2575@naver.com>
2022-03-29 13:34:09 +0900

    Feat: Food 반환 시 image 속성에 base64 문자열 추가

--- Server/search/serializers.py
+++ Server/search/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from menus.models import Food
-
+from image_adapter import ImageIncoder
 class FoodSerializer(serializers.ModelSerializer):
+    # image = serializers.CharField(read_only=True)
+    imageIncoder = ImageIncoder()
+    image = serializers.SerializerMethodField()
     class Meta:
         model = Food
         fields = '__all__'
+    
+    def get_image(self, obj):
+        return self.imageIncoder.getBase64String(obj.foodName)
