861656cb18bb258c795a1e51a446b943ca052563
onehousesilver <onehousesilver@naver.com>
2022-04-01 01:22:58 +0900

    Feat: 음식검색 페이지 UI 및 CSS, 반응형 구현

--- Client/src/components/SearchBar.vue
+++ Client/src/components/SearchBar.vue
@@ -1,33 +1,32 @@
 <template>
   <div class="search-bar">
     <h2>찾는 음식이 있으신가요 ?</h2>
-    <input type="text" v-model="keyword" @keyup.enter="searchKeyword">
-    <button 
-      class="material-icons search-btn"
-      @click="searchKeyword"
-    >search</button>
+    <input type="text" v-model="keyword" @keyup.enter="searchKeyword" />
+    <button class="material-icons search-btn" @click="searchKeyword">
+      search
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchBar',
+  name: "SearchBar",
   data() {
     return {
       keyword: null,
-    }
+    };
   },
   methods: {
     searchKeyword() {
-      this.$emit('searchKeyword', this.keyword)
-    }
-  }
-}
+      this.$emit("searchKeyword", this.keyword);
+    },
+  },
+};
 </script>
 
 <style>
 .search-bar {
-  background: #25AB9B;
+  background: #25ab9b;
   width: 100%;
   height: 10vh;
   display: flex;
@@ -54,14 +53,15 @@ export default {
   border: none;
   font-size: 1vw;
   outline: none;
-  font-family: 'MinSans-Regular';
+  font-family: "MinSans-Regular";
+  background-color: #fff;
 }
 .search-bar input:focus {
-  animation: input-focus .3s ease-in-out;
+  animation: input-focus 0.3s ease-in-out;
   animation-fill-mode: forwards;
 }
 .search-bar input:not(:focus) {
-  animation: input-blur .3s ease-out;
+  animation: input-blur 0.3s ease-out;
 }
 .search-bar .search-btn {
   position: absolute;
---
+++ Client/src/views/SearchView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <BannerBar
+      MainText="음식검색"
+      SubText="diète에서 다양한 음식을 검색해보세요"
+    />
+    <SearchBar />
+    <CategoryBar />
+    <SearchList />
+  </div>
+</template>
+
+<script>
+import BannerBar from "../components/Main/BannerBar.vue";
+import SearchList from "../components/Search/SearchList.vue";
+import CategoryBar from "../components/Search/CategoryBar.vue";
+import SearchBar from "../components/SearchBar.vue";
+export default {
+  components: {
+    BannerBar,
+    SearchBar,
+    SearchList,
+    CategoryBar,
+  },
+};
+</script>
+
+<style></style>
