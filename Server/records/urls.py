6085dd53f9e5f7737ddbda5cdd6a59ebbe78f9a7
rkdms0116 <rkdms0116@gmail.com>
2022-04-06 10:58:43 +0900

    Feat: record 끼니별 영양소 기록조회, 식단 분석 데이터 조회 기능 구현 완료, menu 추천 10개 -> 12개로 변경

--- Server/records/urls.py
+++ Server/records/urls.py
@@ -7,10 +7,10 @@ app_name = 'records'
 urlpatterns = [
     # 주차별 식단 기록 조회
     path('menu/<username>/<int:firstDayOfWeek>/', views.weekly_menus),
-    # 주차별 영양소 기록 조회
-    path('nutrients/<int:userId>/<int:week>/', views.weekly_nutrients),
-    # 식단 분석 데이터 조회
-    path('<int:userId>/', views.data_analysis),
+    # 끼니별 영양소 기록 조회 - 주간
+    path('nutrients/<username>/<int:firstDayOfWeek>/', views.weekly_nutrients),
     # 식단 분석 - 일별 총 칼로리
-    path('<username>/', views.oneday_kcal),
+    path('<username>/', views.data_analysis),
+    # 식단 분석 데이터 조회
+    path('<username>/<int:firstDayOfWeek>/', views.average_nutrients),
 ]
